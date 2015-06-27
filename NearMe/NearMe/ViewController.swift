3c055dd15627b36e4a9e6828287a1a8146cfe780
bryankeller <kellerbryan19@gmail.com>
2015-06-27 15:47:08 -0700

    Add iOS project

---
+++ NearMe/NearMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Bryan Keller on 6/27/15.
+//  Copyright (c) 2015 Greylock Hackathon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
